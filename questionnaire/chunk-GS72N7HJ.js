b8d3c0c05792869f4c48470bca7384f8d0a8620f
m310851010 <310851010@qq.com>
2024-12-17 13:58:24 +0800

    update questionnaire

--- questionnaire/chunk-GS72N7HJ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ZGPTF7QE.js";var t=[{path:"",pathMatch:"full",redirectTo:"dashboard"},{path:"dashboard",loadComponent:()=>import("./chunk-UZNPEDIQ.js")},{path:"**",loadComponent:()=>import("./chunk-OWBNROB7.js").then(o=>o.NotFound404Component)}];export{t as default};
